88f8e259190a531151e197894ad2e79c142e79b6
fikepaci <pacifiquemachine@gmail.com>
2021-08-11 13:42:16 +0200

    add task2clear

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+// function to return current year
+export const getFullYear = () => {
+  return new Date().getFullYear();
+};
+
+export const getFooterCopy = (isIndex) => {
+  let res = undefined;
+  isIndex
+    ? (res = "Holberton School")
+    : (res = "Holberton School main dashboard");
+  return res;
+};
